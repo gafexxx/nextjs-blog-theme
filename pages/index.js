f3953b69a3e8c8e5aefdbd980456b18aa71137c6
gafexxx <146800636+gafexxx@users.noreply.github.com>
2023-10-19 06:22:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,181 @@
-import Link from 'next/link';
-import { getPosts } from '../utils/mdx-utils';
+<!DOCTYPE html>
+<html lang="es">
+<head>
+        <meta chasert="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <tittle>Página Web Fundación Nuevo Amanecer</tittle>
+        <link rel="stylesheet" href="css/estilos.css">
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+        
+    </head>
+<body>
+   <header class="header" id="inicio"> 
+        <img src="img/Hamburguesa.svg" alt="" class="hamburguer">
+        <nav class="menu-navegacion">
+        <a href="#Inico">Inicio</a>
+        <a href="#Quienes somos">Quienes somos</a>
+        <a href="#Portafolio">Portafolio</a>
+        <a href="#Misión">Misión</a>
+        <a href="#Visión">Visión</a>
+        <a href="#Nuestros Principios">Nuestros Principios</a>
+        <a href="#Nuestros valores">Nuestros valores</a>
+        <a href="#Contacto">Contacto</a>
+        </nav>
+     <div class="contenedor head">
+         <h1 class="titulo">FUNDACIÓN SOCIAL Y EDUCATIVA NUEVO AMANECER</h1>
+         <p class="copy">Somos una organización que busca servir a la población mas vulnerable.</p> 
+        </div>
+    </header>
+    <main>
+        
+        <section class="services contenedor" id="Quienes somos">
+            <h2 class="subtitulo">Quiénes somos</h2>
+            <div class="contenedor-servicio"></div>
+            <img src="img/Fundacion-social-y-educativa-nuevo-amanecer.jpg" alt="" >
+            <div class="checklist-servicio">
+                <div class="service">
+                    <h3 class="n-service"><span class="number"></span>¿Quienes Somos?</h3>
+                    <p>Somos una organización sin ánimo de lucro que busca servir a la población más vulnerable de la ciudad de cúcuta y el Departamento Norte de Santander, para brindar un mejor Bienestar a las Familias más necesitadas,mediante la realización de obras benéficas en el sector Comunitario y Educativo con un enfoque inclusivo y con una visión saludable, Educativa, Social, empresarial, cultural,agrícola y ambiental de impacto intersectorial, y en alianza con otras organizaciónes Públicas o Privadas, liderando proyectos en sectores priorizados
 
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Layout, { GradientBackground } from '../components/Layout';
-import ArrowIcon from '../components/ArrowIcon';
-import { getGlobalData } from '../utils/global-data';
-import SEO from '../components/SEO';
+                         </p>
+                </div>           
+            </div>
+            
+        </section>
+     
+        <section class="gallery" id="Portafolio">
+            <div class="contenedor">
+                <h2 class="subtitulo">Galeria</h2>
+                <div class="contenedor-galeria">
+                   <img src="img/foto 1.jpg" alt="" class="img-galeria">
+                   <img src="img/Foto 2.jpg" alt="" class="img-galeria">
+                   <img src="img/foto 3.jpg" alt="" class="img-galeria">
+                   <img src="img/foto 4.jpg" alt="" class="img-galeria">
+                   <img src="img/foto-5.jpg" alt="" class="img-galeria">
+                   <img src="img/foto-6.jpg" alt="" class="img-galeria">
+                   <img src="img/foto-777.jpg" alt="" class="img-galeria">
+                   <img src="img/foto-8.jpg" alt="" class="img-galeria">
+                   <img src="img/foto-9.jpg" alt="" class="img-galeria">
+                   <img src="img/foto-10.jpg" alt="" class="img-galeria">
+                </div>
+            </div>
 
-export default function Index({ posts, globalData }) {
-  return (
-    <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
-      <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
-        </h1>
-        <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
+        <section class="imagen-light">
+                <img src="img/CloseLight.png" alt="" class="close">
+                <img src="" alt="" class="agregar-imagen">
+        </section>
+            
+        </section>
+        </section>
+
+       
+            <section class="Mision contenedor" id="Misión">
+                <h3 class="n-service"><span class="number"></span>¿Cual es nuestra Misión?</h3>
+                <div class="contenedor-servicio"></div>
+                <img src="img/Misión.png" alt="" >
+                <div class="checklist-servicio">
+                    <div class="service"> 
+                        <p>Contribuimos al Bienestar de los niños, jovenes, adultos y familias que se encuentran en vulnerabilidad, para ofrecer oportunidades educativas y laborales para vivir en un entorno mas digno, generando progreso a través de la educacion, desarrollo social y el cuidado del medio ambiente.
+    
+                        </p>
+                    </div>
+            </section>
+
+            <section class="Vision contenedor" id="Visión">
+                <h3 class="n-service"><span class="number"></span>¿Cual es nuestra visión?</h3>
+                <div class="contenedor-servicio"></div>
+                <img src="img/Visión.png" alt="" >
+                <div class="checklist-servicio">
+                    <div class="service">
+                       
+                        <p>Ser una organización de desarrollo social de alto impacto, reconocida en la ciudad de Cúcuta y el Departamento Norte de Santander, por su esfuerzo e incidencia en la transformacion de una sociedad mas digna y garante en oportunidades para la promocion de la educación, progreso y cuidado del medio ambiente.
+    
+                        </p>
+                    </div>
+            </section>
+
+    
+
+            <section class="services contenedor" id="Nuestros Principios">
+              
+                <div class="contenedor-servicio"></div>
+                <h3 class="n-service"><span class="number"></span>¿Cuales son nuestros principios?</h3>
+                <img src="img/Nuestros-Principios.png" alt="" >
+                <div class="checklist-servicio">
+                    <div class="service">
+                       
+                        <p>La Fundación se guiará por los siguientes principios:
+                            Democracia, participación, equidad, igualda, diversidad, justicia, creatividad  y en general por los derechos fundamentales de los seres humanos consagrados en la Constitución política de Colombia y el derecho internacional.
+    
+                             </p>
+                    </div>        
+        </section>
+            
+        <section class="services contenedor" id="Nuestros valores">
+              
+                <div class="contenedor-servicio"></div>
+                <h3 class="n-service"><span class="number"></span>¿Cuales son nuestros valores?</h3>
+                <img src="img/Valores.png" alt="" >
+                <div class="checklist-servicio">
+                    <div class="service">
+        
+                        <p>La Fundación se guiará por los siguientes valores:
+                            Respeto, honestidad, responsabilidad, solidaridad, transparencia, integridad y la calidad humana.
+    
+                             </p>
+                    </div>
+    
+
+
+
+
+
+
+                
+                </div>
+        </section>
+    </main>
+    <footer id="Contacto">
+        <div class="contenerdor footer-content">
+            <div class="contact-us">
+                <h2 class="brand">Fundación Nuevo Amanecer</h2>
+                <p>Somos una organización que busca servir a  la población mas vulnerable.</p>
+
+                <p>Mas información en nuestras redes sociales.
+                </p>
+            </div>
+
+            <div class="social-media">
+                <a href="https://www.instagram.com/fund_nuevo_amanecer/" class="social-media-icon">
+                    <i class='bx bxl-instagram' ></i>    
                 </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-      <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
-    </Layout>
-  );
-}
 
-export function getStaticProps() {
-  const posts = getPosts();
-  const globalData = getGlobalData();
+                <a href="https://www.facebook.com/fundelina.selenina" class="social-media-icon">
+                    <i class='bx bxl-facebook-circle' ></i>   
+                </a>
 
-  return { props: { posts, globalData } };
-}
+                <a href="https://wa.link/suw46u" class="social-media-icon">  
+                    <i class='bx bxl-whatsapp'></i>
+                </a>
+            </div>
+
+            <div class="line"></div>
+            <div class="mas-información">
+              
+            </div>
+            
+
+        </div>
+    </footer>
+
+    <script src="js/menu.js"></script>
+    <script src="js/lightbox.js"></script>
+
+       
+      
+    </body> 
+</html>
+
